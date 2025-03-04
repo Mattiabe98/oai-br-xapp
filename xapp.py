51360f6cb51df6635daf93c3a6db682797f5e4cd
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-04 15:58:36 +0100

    Update xapp.py

--- xapp.py
+++ xapp.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
                     time.sleep(1)
         elif sm_name == "GTP":
             for i in range(0, len(conn)):
-                if conn[i].id.type == 9:
+                if conn[i].id.type == 7:
                     print("Subscribing to GTP..")
                     # GTP
                     gtp_cb = GTPCallback()
